e3160f8603f90b94c9a2d3ca999210315821385d
Carlitos Rodriguez <78374556+CarlRod2001@users.noreply.github.com>
2022-03-20 17:50:13 -0400

    Update GrowArray.cc

--- GrowArray.cc
+++ GrowArray.cc
@@ -5,96 +5,93 @@ using namespace std;
 
 class GrowArray{
 private:
-    int *p;
-    uint32_t size;		// the number of elements used
-    uint32_t capacity;		// the amount of memory
-    void checkGrow (){
-        int *old = p;
-        p = new int[2 * capacity];
-        for (int i = 0; i < size; i++){
-            p[i] = old[i];
-        }
-        delete[] old;
-        capacity = 2 * capacity;
+  int *p;
+  uint32_t size;		// the number of elements used
+  uint32_t capacity;		// the amount of memory
+  void checkGrow (){
+    int *old = p;
+    p = new int[2 * capacity];
+    for (int i = 0; i < size; i++){
+	    p[i] = old[i];
     }
+    delete[] old;
+    capacity = 2 * capacity;
+}
 public:
-    GrowArray (){
-        p = nullptr;
-        size = 0;
-        capacity=1;
+  GrowArray (){
+    p = nullptr;
+    size = 0;
+    capacity=1;
+  }
+  void createArray (int v){
+    if (size == capacity){
+	    checkGrow ();
     }
-    //GrowArray ( u i n t 3 2 t i n i t i a l C a p a c i t y ) ;
-    void createArray (int v){
-        if (size == capacity){
-            checkGrow ();
-        }
-        int *temp;
-        temp = new int[size+1];
-        for (int i = 0; i < size; i++){
-            temp[i] = p[i];
-        }
-        temp[size] = v;
-        delete[] p;
-        p=temp;
-        size++;
+    int *temp;
+    temp = new int[size+1];
+    for (int i = 0; i < size; i++){
+	    temp[i] = p[i];
     }
-    void addEnd (int v){
-        if (size == capacity){
-            checkGrow ();
-        }
-        p[size]=v;
-        size++;
+    temp[size] = v;
+    delete[] p;
+    p=temp;
+    size++;
+  }
+  void addEnd (int v){
+    if (size == capacity){
+	    checkGrow ();
     }
-
-    void addStart (int v){
-        if (size == capacity){
-            checkGrow ();
-        }
-        int *old = p;
-        p = new int[size + 1];
-        for (int i = 0; i < 0; i++)
-            p[i] = old[i];
-        p[0] = v;
-        for (int i = 0; i < size; i++)
-            p[i + 1] = old[i];
-        delete[]old;
-        size++;
+    p[size]=v;
+    size++;
+  }
+  void addStart (int v){
+    if (size == capacity){
+	checkGrow ();
     }
-    void removeStart (){
-        if (size > 0) {
-            for (int i = 0; i < size - 1; i++) {
-                p[i] = p[i + 1];
-            }
-            p[size - 1] = 0;
-            size--;
+    int *old = p;
+    p = new int[size + 1];
+    for (int i = 0; i < 0; i++)
+      p[i] = old[i];
+    p[0] = v;
+    for (int i = 0; i < size; i++)
+      p[i + 1] = old[i];
+    delete[]old;
+    size++;
+  }
+  void removeStart (){
+    if (size > 0) {
+        for (int i = 0; i < size - 1; i++) {
+        p[i] = p[i + 1];
         }
+        p[size - 1] = 0;
+        size--;
     }
-    void removeEnd (){
-        if (size > 0){
-            p[size - 1] = 0;
-            size--;
-        }
+  }
+  void removeEnd (){
+    if (size > 0){
+	    p[size - 1] = 0;
+	    size--;
     }
-
-    void removeEvens (){
-        uint32_t j = 0;
-        for (int i = 0; i < size; i++){
-            if (p[i] % 2 != 0){
-                p[j++] = p[i];
-            }
-        }
-        j = (size)-j;
-        for (int i = 0; i < j; i++){
-            p[size - 1] = 0;
-            size--;
-        }
+  }
+  void removeEvens (){
+    uint32_t j = 0;
+    for (int i = 0; i < size; i++){
+	    if (p[i] % 2 != 0){
+	    p[j++] = p[i];
+	  }
     }
-    void printArr (){
-        for (int i = 0; i < size; i++){
-            cout << p[i] << " ";
-        }
-        // cout << endl; cout << "size: " << size << endl; cout << "capacity: " << capacity <<  endl;
+    j = (size)-j;
+    for (int i = 0; i < j; i++){
+        p[size - 1] = 0;
+	    size--;
     }
+  }
+  void printArr (){
+    for (int i = 0; i < size; i++){
+	    cout << p[i] << " ";
+    }
+    // cout << endl; cout << "size: " << size << endl; cout << "capacity: " << capacity <<  endl;
+  }
 };
 
 int main (){
@@ -107,18 +104,18 @@ int main (){
         a.addEnd (i);
     a.removeStart();
     // end of creation of the array
-
+    
     for (int i = 0; i < 100000; i++)
-        a.addEnd(i);
+		a.addEnd(i); 
 
-    a.addStart(5);
+	a.addStart(5);
 
-    for (int i = 0; i < 90500; i++)
-        a.removeEnd();
+	for (int i = 0; i < 90500; i++)
+		a.removeEnd();
 
-    for (int i = 0; i < 9000; i++)
-        a.removeStart();
-
-    a.removeEvens();
-    a.printArr();
+	for (int i = 0; i < 9000; i++)
+		a.removeStart();
+		
+	a.removeEvens();
+	a.printArr();
 }
